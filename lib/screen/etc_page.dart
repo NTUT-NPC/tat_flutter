abc9ea9a1ddb7ace2f13cb33900e029a92062970
yaoandy107 <yaoandy107@gmail.com>
2019-05-31 14:37:43 +0800

    Add etc blank page

---
+++ lib/screen/etc_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EtcPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'TAT - 其它',
+          style: TextStyle(letterSpacing: 2),
+        ),
+      ),
+    );
+  }
+}

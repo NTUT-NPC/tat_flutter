1f01e4c4e765050895bf522a6654e44868fa83f9
yaoandy107 <yaoandy107@gmail.com>
2019-05-31 14:37:32 +0800

    Add credit blank page

---
+++ lib/screen/credit_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreditPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'TAT - 學分',
+          style: TextStyle(letterSpacing: 2),
+        ),
+      ),
+    );
+  }
+}

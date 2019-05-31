48ae676094518f04279a6084fbc96d2e7aa92d77
yaoandy107 <yaoandy107@gmail.com>
2019-05-31 14:30:14 +0800

    Add onTab and modify architecture

---
+++ lib/screen/curriculum_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CurriculumPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'TAT - 課表',
+          style: TextStyle(letterSpacing: 2),
+        ),
+      ),
+    );
+  }
+}
--- lib/tab_scaffold.dart
+++ lib/tab_scaffold.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tat_flutter/screen/curriculum_page.dart';
 
 class TabScaffold extends StatefulWidget {
   @override
@@ -7,36 +8,52 @@ class TabScaffold extends StatefulWidget {
 
 class _TabScaffoldState extends State<TabScaffold> {
   final _bottomNavigationColor = Colors.blueGrey;
+  int _selectedIndex = 0;
+  final pages = [
+    CurriculumPage(),
+    CurriculumPage(),
+    CurriculumPage(),
+    CurriculumPage()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'TAT',
-          style: TextStyle(letterSpacing: 4),
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: _bottomNavigationColor),
+              title: Text(
+                '課表',
+                style: TextStyle(color: _bottomNavigationColor),
+              )),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today, color: _bottomNavigationColor),
+              title: Text(
+                '行事曆',
+                style: TextStyle(color: _bottomNavigationColor),
+              )),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.assessment, color: _bottomNavigationColor),
+              title: Text(
+                '學分',
+                style: TextStyle(color: _bottomNavigationColor),
+              )),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.menu, color: _bottomNavigationColor),
+              title: Text(
+                '其它',
+                style: TextStyle(color: _bottomNavigationColor),
+              )),
+        ],
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: _bottomNavigationColor),
-            title: Text(
-              '課表',
-              style: TextStyle(color: _bottomNavigationColor),
-            )),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today, color: _bottomNavigationColor),
-            title: Text(
-              '行事曆',
-              style: TextStyle(color: _bottomNavigationColor),
-            )),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.assessment, color: _bottomNavigationColor),
-            title: Text(
-              '學分',
-              style: TextStyle(color: _bottomNavigationColor),
-            )),
-      ]),
+      body: pages[_selectedIndex],
     );
   }
 }

6d39a60f23d25f1350d48dd990effc5eeb533827
yaoandy107 <yaoandy107@gmail.com>
2019-06-02 17:35:43 +0800

    Add constants.dart

---
+++ lib/utils/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/painting.dart';
+
+const EdgeInsets kScreenPadding = EdgeInsets.all(32);
+const BorderRadius kBorderRadius = BorderRadius.all(Radius.circular(5));
+
+const double kSmallFontSize = 14;
+const double kLargeFontSize = 20;
+const double kExtraLargeFontSize = 32;
